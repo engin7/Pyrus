e94d9f9b196d7be226c9b5317eae6f9524b6f224
Engin KUK <kuk.engin@gmail.com>
2020-09-09 12:23:10 +0300

    Initial Commit

---
+++ Pyrus/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Pyrus
+//
+//  Created by Engin KUK on 9.09.2020.
+//  Copyright © 2020 Engin KUK. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pyrus/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Pyrus
+//
+//  Created by Engin KUK on 9.09.2020.
+//  Copyright © 2020 Engin KUK. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
